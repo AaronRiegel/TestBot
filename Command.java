df2c06ee548851b35b2a39f1c0ef837ccea8af33
AaronRiegel <riegelaaron@gmail.com>
2018-04-04 13:13:51 -0400

    Initial Commit

---
+++ Command.java
@@ -0,0 +1,57 @@
+package bot.discord;
+
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Command extends ListenerAdapter {
+
+    public void onMessageReceived(MessageReceivedEvent e) {
+        if(e.getMessage().getContent().startsWith("!")) {
+            String[] args = e.getMessage().getContent().replaceFirst( "!", "").split( " ");
+
+
+
+            switch(args[0]) {
+                case "hello": help(e.getMessage()); System.out.println("says hello"); break;
+                case "delete" : delete(e.getMessage()); System.out.println("deleting memes"); break;
+            }
+        }
+    }
+
+
+    private void help(Message msg) {
+        String user = msg.getAuthor().getName().toString();
+        msg.getChannel().sendMessage( "Hello "+ user).queue(m-> {
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    //msg.delete().queue();
+                }
+            }, 5000);
+        });
+
+    }
+
+    private void delete(Message msg) {
+        String user = msg.getAuthor().getName().toString();
+        msg.getChannel().sendMessage("Deleting " + user + "'s data from their hard drive\nbeep\nboop").queue(m-> {
+            ;
+
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    msg.delete().queue();
+                }
+            }, 5000);
+        });
+    }
+
+
+}
---
+++ Main.java
@@ -0,0 +1,36 @@
+package bot.discord;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static JDA jda;
+
+    public static void main(String args[]) {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        builder.setToken("Your token here");
+        builder.setAutoReconnect(true);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.addEventListener(new Command());
+
+       
+
+
+
+        try{
+            jda = builder.buildBlocking();
+        }catch(LoginException e) {
+            e.printStackTrace();
+        }catch(InterruptedException e) {
+            e.printStackTrace();
+        } catch(RateLimitedException e){
+            e.printStackTrace();
+        }
+    }
+}
